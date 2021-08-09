fc856759eb015c137122b8bdac7f115ae65df8fa
valed-dm <dmvaled@gmail.com>
2021-08-09 20:27:49 +0300

    Start a Working Express Server Passed

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+console.log("Hello World");
+app.get("/", function(req, res) {
+  res.send("Hello Express");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
