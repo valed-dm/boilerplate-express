40ee1a1d0cb85218910f44377195142aeb6c5f31
valed-dm <dmvaled@gmail.com>
2021-08-11 19:09:25 +0300

    Serve JSON on a Specific Route Passed

--- myApp.js
+++ myApp.js
@@ -4,5 +4,9 @@ console.log("Hello World");
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-app.use("/public", express.static(__dirname + "/public"))
+app.get("/json", function(req, res) {
+  res.json({ message: "Hello json" });
+});
+app.use("/public", express.static(__dirname + "/public"));
+
 module.exports = app;
