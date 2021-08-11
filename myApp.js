2ce42304226bf02326c03cf09c1d3f0dc39be71a
valed-dm <dmvaled@gmail.com>
2021-08-11 14:58:16 +0300

    Serve Static Assets Passed

--- myApp.js
+++ myApp.js
@@ -4,5 +4,5 @@ console.log("Hello World");
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
-
+app.use("/public", express.static(__dirname + "/public"))
 module.exports = app;
