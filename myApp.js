65d67d3891e7169b368f2fa0541e52e205e4834b
valed-dm <dmvaled@gmail.com>
2021-08-11 13:26:09 +0300

    Serve an HTML File completed

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 console.log("Hello World");
 app.get("/", function(req, res) {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
